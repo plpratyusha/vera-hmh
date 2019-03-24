398db5a09a009156cc7a646d8cd9a0e7037474f4
plpratyusha <plpratyusha@gmail.com>
2019-03-23 21:27:05 -0700

    First commit

--- config/credentials.js
+++ config/credentials.js
@@ -1,6 +1,6 @@
 module.exports = {
-  GITHUB_CLIENT_ID: '',
-  GITHUB_CLIENT_SECRET: '',
-  GITHUB_CALLBACK_URL: '',
-  DBURL: '',
+  GITHUB_CLIENT_ID: '286b9afe504100163e71',
+  GITHUB_CLIENT_SECRET: '30716da5ac40eaa41eb7d80dd9e31a55ebba833c',
+  GITHUB_CALLBACK_URL: 'https://localhost:3030/api/user/authViaGitHub/callback',
+  DBURL: 'mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb',
 };
